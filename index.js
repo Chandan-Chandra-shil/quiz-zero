be52c054dea1f6cb33bfd6fb5fc37d8e31b4b3ac
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-03-15 22:36:14 +0600

    first error again fixed

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const displayQuiz = (data) => {
 
 // EventListener for quiz submit button
 // first error hare 
-document.querySelector("submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
